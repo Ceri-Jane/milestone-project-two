8c616978473b16f5eb194c345a119a5561b26ef9
Ceri-Jane <ceri.jane.89@gmail.com>
2025-06-24 19:23:53 +0100

    add a counter for amount of flags left and set bomb amount for game

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,14 @@
 // Using this custom script in head element so adding this DOM content listener to run once everything in body element has loaded
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
+    const flagsLeft = document.querySelector('#flags-left')
     const width = 10
+    //set amount of bombs to be used in the game - use let instead of const as this can then be changed in the future
+    let bombAmount = 20
 
     //Create game board
     function createBoard() {
+        flagsLeft.innerHTML = bombAmount
 
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
