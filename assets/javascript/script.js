3923ec235386e10fc15907629daa780093fa8451
Ceri-Jane <ceri.jane.89@gmail.com>
2025-06-24 19:01:04 +0100

    creat event listener to load custom js and create function for createboard

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+// Using this custom script in head element so adding this DOM content listener to run once everything in body element has loaded
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const width = 10
+
+    //Create game board
+    function createBoard() {
+
+    }
+
+})
